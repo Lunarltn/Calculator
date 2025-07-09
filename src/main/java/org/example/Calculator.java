2ed5fc83d08d543fb5656840f3cbb6b4ab4495c1
kisu kim <qpsrltn@gmail.com>
2025-07-09 16:53:48 +0900

    Init

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.awt.Frame;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+    }
+}
