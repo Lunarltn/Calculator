ef5b373273f9b0fe167ec1b74f805de95fd5c38e
kisu kim <qpsrltn@gmail.com>
2025-07-09 18:46:47 +0900

    사칙연산 기호를 입력받기

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -10,6 +10,8 @@ public class Calculator {
         int n = sc.nextInt();
         System.out.println("두 번째 숫자를 입력하세요: ");
         int m = sc.nextInt();
-        
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        char c = sc.next().charAt(0);
+
     }
 }
