5e625bc2bc2353dccae5c3d3a45a98d78e7d9f00
kisu kim <qpsrltn@gmail.com>
2025-07-09 18:57:39 +0900

    [feat] 사칙연산 기호를 사용하여여 연산 후 결과를 출력

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -13,5 +13,27 @@ public class Calculator {
         System.out.println("사칙연산 기호를 입력하세요: ");
         char c = sc.next().charAt(0);
 
+        int result = 0;
+        switch (c) {
+            case '+':
+                result = n + m;
+                break;
+            case '-':
+                result = n - m;
+                break;
+            case '*':
+                result = n * m;
+                break;
+            case '/':
+                if (m == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
+                    break;
+                }
+                result = n / m;
+                break;
+        }
+
+        if (!(c == '/' && m == 0)) // 분모가 0인 나눗셈 처리
+            System.out.println("결과: " + result);
     }
 }
