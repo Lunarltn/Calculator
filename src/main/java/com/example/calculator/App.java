18b867cfb699a0f2f3ce86957bb595eae41a6b36
kisu kim <qpsrltn@gmail.com>
2025-07-10 19:18:39 +0900

    [feat] ArithmeticCalculator 클래스 제네릭화, 여러 타입의 값을 전달받아도 연산이 수행되도록 수정, 결과값을 정수 실수에 맞게  출력하도록 수정

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -6,19 +6,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        ArithmeticCalculator calculator = new ArithmeticCalculator();
+        ArithmeticCalculator<Number> calculator = new ArithmeticCalculator<>();
         Scanner sc = new Scanner(System.in);
 
         do {
             System.out.println("첫 번째 숫자를 입력하세요: ");
-            int n = safeNextInt(sc);
+            Number n = safeNextNumber(sc);
             System.out.println("두 번째 숫자를 입력하세요: ");
-            int m = safeNextInt(sc);
+            Number m = safeNextNumber(sc);
             System.out.println("사칙연산 기호를 입력하세요: ");
-            OperatorType c = safeNextOperation(sc);
+            OperatorType c = safeNextOperator(sc);
 
             try {
-                int result = calculator.calculate(n, m, c);
+                Number result = calculator.calculate(n, m, c);
                 System.out.println("결과: " + result);
             } catch (ArithmeticException e) {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
@@ -28,7 +28,7 @@ public class App {
         while (!sc.next().equals("exit"));
 
         System.out.println("연산 된 결과값들: " + calculator.getResults());
-        List<Integer> results = calculator.getResults();
+        List<Number> results = calculator.getResults();
         results.add(1000);
         calculator.setResults(results);
         System.out.println("1000을 넣고 변경 된 결과값: " + calculator.getResults());
@@ -36,18 +36,38 @@ public class App {
         System.out.println("가장 먼저 저장된 데이터 삭제 후 변경 된 결과값: " + calculator.getResults());
     }
 
-    public static int safeNextInt(Scanner sc) {
+    public static Number safeNextNumber(Scanner sc) {
         while (true) {
-            try {
-                return sc.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("[Error] 숫자를 입력해 주세요.");
-                sc.nextLine();
+            String input = sc.next();
+            if (isInteger(input)) {
+                return Integer.parseInt(input);
+            } else if (isDouble(input)) {
+                return Double.parseDouble(input);
             }
+            System.out.println("[Error] 숫자를 입력해 주세요.");
+            sc.nextLine();
         }
     }
 
-    public static OperatorType safeNextOperation(Scanner sc) {
+    static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    static boolean isDouble(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static OperatorType safeNextOperator(Scanner sc) {
         while (true) {
             try {
                 char c = sc.next().charAt(0);
--- src/main/java/com/example/calculator/ArithmeticCalculator.java
+++ src/main/java/com/example/calculator/ArithmeticCalculator.java
@@ -3,40 +3,40 @@ package com.example.calculator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArithmeticCalculator {
+public class ArithmeticCalculator<T extends Number> {
     // 연산 결과 저장
-    private List<Integer> results = new ArrayList<Integer>();
+    private static List<Number> results = new ArrayList<>();
 
-    public List<Integer> getResults() {
+    public List<Number> getResults() {
         return results;
     }
 
-    public void setResults(List<Integer> results) {
+    public void setResults(List<Number> results) {
         this.results = results;
     }
 
-    public int calculate(int n, int m, OperatorType o) throws ArithmeticException {
-        int result = 0;
-        switch (o) {
-            case SUM:
-                result = n + m;
-                break;
-            case SUB:
-                result = n - m;
-                break;
-            case MUL:
-                result = n * m;
-                break;
-            case DIV:
-                try {
-                    result = n / m;
-                } catch (ArithmeticException e) {
-                    throw e;
-                }
-                break;
+    public Number calculate(T n, T m, OperatorType o) throws ArithmeticException {
+        double result = calculate(n.doubleValue(), m.doubleValue(), o);
+
+        if (result % 1 == 0) {
+            results.add((int) result);
+            return (int) result;
         }
         results.add(result);
         return result;
+
+    }
+
+    double calculate(double n, double m, OperatorType o) throws ArithmeticException {
+        return switch (o) {
+            case SUM -> n + m;
+            case SUB -> n - m;
+            case MUL -> n * m;
+            case DIV -> {
+                if (m == 0) throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                yield n / m;
+            }
+        };
     }
 
     public void removeResult() {
